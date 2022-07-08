fb0e1307807ad6ccb4553c4a271deaeda71397bb
Wonwoo Jin <jamesjin06@gmail.com>
2022-07-09 00:01:39 +0900

    모딩 시작

---
+++ src/main/java/git/cozy06/mod/mixin/ExampleMixin.java
@@ -0,0 +1,14 @@
+package git.cozy06.mod.mixin;
+
+import git.cozy06.mod.Advanturous;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {Advanturous.LOGGER.info("This line is printed by an example mod mixin!");}
+}
